6428d9b797e0595a9fa5ce550f54db079d6d8cbc
zzsnn <1620993037@qq.com>
2020-05-01 21:16:27 +0800

    Site updated: 2020-05-01 21:16:22

--- js/music.js
+++ js/music.js
@@ -1,3 +1,3 @@
-// build time:Fri May 01 2020 20:45:34 GMT+0800 (GMT+08:00)
+// build time:Fri May 01 2020 21:15:11 GMT+0800 (GMT+08:00)
 const ap=new APlayer({container:document.getElementById("aplayer"),fixed:true,autoplay:true,theme:"#FADFA3",audio:[{name:"纪念用生命托起世界的人",artist:"小石头和孩子们",url:"http://antiserver.kuwo.cn/anti.s?useless=/resource/&format=mp3&rid=MUSIC_94162625&response=res&type=convert_url&",cover:"https://vthumb.ykimg.com/054101015E416AA1ADCA61A8D7CA34B0"},{name:"你的答案",artist:"阿冗",url:"https://sharefs.yun.kugou.com/202004220941/fcf02eb6e703c080805e5846bba76ce9/G171/M07/1F/05/i5QEAF26pGeAMPHiADWzkIj97AU712.mp3",cover:"http://cdn.daweijita.com/2020/01/arong_nidedaan_guitar.jpg"}]});
 //rebuild by neat 
